f074beb11a85d9c574acf0eeef127b20dd2c3702
Enviction <schmidtjonas662@gmail.com>
2025-03-28 15:29:58 +0100

    feat: add ADict

--- pyobj.py
+++ pyobj.py
@@ -17,3 +17,21 @@ class D:
 
 	def __str__(self):
 		return str(self.__dict__)
+
+class ADict:
+	def __init__(self, **kwargs):
+		self.__dict__ = kwargs
+
+	def __getattribute__(self, name):
+		if name == "__dict__":
+			return super().__getattribute__(name)
+		return self.__dict__[name]
+
+	def __setattr__(self, name, value):
+		if name == "__dict__":
+			super().__setattr__(name, value)
+		else:
+			self.__dict__[name] = value
+
+	def __str__(self):
+		return str(self.__dict__)
